e280dbdf6afba236d389b8429d98d938324621b9
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-05-18 23:11:54 +0300

    fix bug => add *ngIf="($observable | async)" into new-page.component.html

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -21,23 +21,18 @@ export class NewPageComponent implements OnInit {
   constructor(private router: Router) {
   }
 
-
-
   async ngOnInit() {
     //console.log(this.router.url);
     let movieId = this.router.url.slice(7)
-    this.movie  = await instance.get<movieType>(`/movie/${movieId}?api_key=${API_KEY}&${languageURL}`).then(resp=>{
+    this.movie = await instance.get<movieType>(`/movie/${movieId}?api_key=${API_KEY}&${languageURL}`).then(resp => {
       return resp.data
-    }).catch(err=>this.router.navigate([`/error`])
+    }).catch(err => this.router.navigate([`/error`])
+      //console.log(`error`,error);
+    );
 
-        //console.log(`error`,error);
-
-
-      );
-
-    this.$observable = instance.get<movieType>(`/movie/${movieId}?api_key=${API_KEY}&${languageURL}`).then(resp=>{
+    this.$observable = instance.get<movieType>(`/movie/${movieId}?api_key=${API_KEY}&${languageURL}`).then(resp => {
       return resp.data
-    }).catch(error=>console.log(`Упс! что-то пошло не так:`, error.message))
+    }).catch(error => console.log(`Упс! что-то пошло не так:`, error.message))
     // console.log(movieId)//675353
     // console.log(this.movie)
     // console.log(this.movie?.backdrop_path)
